c95e6a56d091880b3c49802ab9c2e593b901d971
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-03-03 16:17:23 +0530

    connection done

--- index.js
+++ index.js
@@ -18,9 +18,18 @@ app.post('/', (request, response) => {
 //importing mongoose and connecting db
 const mongoose = require('mongoose')
 //connection code
-mongoose.connect('mongodb://localhost:27017/AayushFirstBackendDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect('mongodb://localhost:27017/AayushFirstBackendDB').then(() => {
     console.log("***database connected!!!");
+}).catch((err) => {
+    console.log(err);
 })
+
+
+// mongoose.connect('mongodb://localhost:27017/AayushFirstBackendDB', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then(() => {
+//     console.log("***database connected!!!");
+// }).catch((err)=>{
+//     console.log(err);
+// })
